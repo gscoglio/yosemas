2081c58580a8b214ac72e0a7ec8deec90c7b2819
German Scoglio <german.scoglio@gmail.com>
2014-04-05 13:20:56 -0300

    Updating Localidades in Santa Fe

---
+++ sqls/update_localidades_santa_fe.sql
@@ -0,0 +1,4 @@
+update localidades set nombre_ciudad = "CAÑADA ROSQUIN" where id = 2233;
+update localidades set nombre_ciudad = "CAÑADITA" where id = 2117;
+update localidades set nombre_ciudad = "CAÑADA DE GOMEZ" where id = 1445;
+update localidades set nombre_ciudad = "CAÑADA DEL UCLE" where id = 1880;
